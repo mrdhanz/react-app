91e78c332ec596c32267d128e6aa2bb14c106866
mrdhanz <dandigilangr99@gmail.com>
2024-09-02 21:57:23 +0700

    update .env

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App Client: <code>{clientName}</code>.
+          App Client: <code><b>{clientName}</b></code>.
         </p>
         <a
           className="App-link"
